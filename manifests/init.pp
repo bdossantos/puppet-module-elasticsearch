1cbd945777a0c79e3adddd9f8272683afd4d89c5
Benjamin Dos Santos <benj.ds@gmail.com>
2012-07-10 17:01:29 +0200

    add params

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,12 @@
 class elasticsearch(
   $version  = '0.19.8',
-  $cwd = '/usr/local/src'
+  $cwd = '/usr/local/src',
+  $cluster_name = 'elasticsearch',
+  $node_master = true,
+  $node_data = true,
+  $index_number_of_shards = 5,
+  $index_number_of_replicas = 1,
+  $bind_host = '127.0.0.1'
 ) {
   
   $download = "https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-${elasticsearch::version}.deb"
